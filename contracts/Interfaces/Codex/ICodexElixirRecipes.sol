f3d7b9f39ceed1ec582841f7b5cec1a158fc3732
executionreverted <canersevince3@gmail.com>
2022-03-16 07:32:59 +0300

    fix contract keyword in interfaces

--- contracts/Interfaces/Codex/ICodexElixirRecipes.sol
+++ contracts/Interfaces/Codex/ICodexElixirRecipes.sol
@@ -3,12 +3,12 @@ import "../GameObjects/IGameObjects.sol";
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-contract ICodexElixirRecipes {
+interface ICodexElixirRecipes {
 
-    function lifetime_by_tier(uint _id) public pure returns (uint256);
+    function lifetime_by_tier(uint _id) external pure returns (uint256);
 
     function getAllElixirs(uint8 tier) external pure returns (GameObjects.ElixirRecipe[] memory);
 
-    function recipe(uint _id, uint8 _tier) public pure returns (GameObjects.ElixirRecipe memory _recipe);
+    function recipe(uint _id, uint8 _tier) external pure returns (GameObjects.ElixirRecipe memory _recipe);
 
 }
--- contracts/Interfaces/Codex/ICodexExpCards.sol
+++ contracts/Interfaces/Codex/ICodexExpCards.sol
@@ -4,20 +4,20 @@ import "../NonFungibles/EXP/EXPCardLib.sol";
 
 pragma solidity 0.8.7;
 
-contract ICodexExpCards {
+interface ICodexExpCards {
 
     function getAllCards() external view returns (EXPCardLib.EXPCard[] memory);
 
-    function item(uint _id) public view returns (EXPCardLib.EXPCard memory _misc);
+    function item(uint _id) external view returns (EXPCardLib.EXPCard memory _misc);
 
-    function ExpCard1 () public view returns(EXPCardLib.EXPCard memory _expCard);
+    function ExpCard1 () external view returns(EXPCardLib.EXPCard memory _expCard);
 
-    function ExpCard2 () public view returns(EXPCardLib.EXPCard memory _expCard);
+    function ExpCard2 () external view returns(EXPCardLib.EXPCard memory _expCard);
 
-    function ExpCard3 () public view returns(EXPCardLib.EXPCard memory _expCard);
+    function ExpCard3 () external view returns(EXPCardLib.EXPCard memory _expCard);
 
-    function ExpCard4 () public view returns(EXPCardLib.EXPCard memory _expCard);
+    function ExpCard4 () external view returns(EXPCardLib.EXPCard memory _expCard);
 
-    function ExpCard5 () public view returns(EXPCardLib.EXPCard memory _expCard);
+    function ExpCard5 () external view returns(EXPCardLib.EXPCard memory _expCard);
 
 }
