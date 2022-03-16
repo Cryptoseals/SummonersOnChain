514a87a3d1eb529a965a6fa805354f04ffca6320
executionreverted <canersevince3@gmail.com>
2022-03-16 07:26:36 +0300

    codex interfaces for recipes, exp cards, misc, random

---
+++ contracts/Interfaces/Codex/ICodexElixirRecipes.sol
@@ -0,0 +1,14 @@
+import "../GameObjects/GameObjects.sol";
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract ICodexElixirRecipes {
+
+    function lifetime_by_tier(uint _id) public pure returns (uint256);
+
+    function getAllElixirs(uint8 tier) external pure returns (GameObjects.ElixirRecipe[] memory);
+
+    function recipe(uint _id, uint8 _tier) public pure returns (GameObjects.ElixirRecipe memory _recipe);
+
+}
