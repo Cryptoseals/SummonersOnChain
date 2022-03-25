1ab32cdf8501b06ecbc7bfe4ce3e25999726a1c8
executionreverted <canersevince3@gmail.com>
2022-03-25 20:39:46 +0300

    adventure battle stats now has a type of monster

--- contracts/Interfaces/GameObjects/IAdventure.sol
+++ contracts/Interfaces/GameObjects/IAdventure.sol
@@ -3,19 +3,24 @@ pragma solidity ^0.8.0;
 
 interface IAdventure {
 
-    struct AdventureMetadata {
+    struct Metadata {
         string name;
         string description;
     }
 
-    struct AdventureLevel {
-        AdventureMetadata Metadata;
+    struct Monster {
+        Metadata MonsterMetadata;
         GameObjects.Stats EnemyStats;
         GameObjects.GeneratedStats EnemyGeneratedStats;
         GameObjects.ElementalStats EnemyElementalStats;
+        GameObjects.Element damageType;
+    }
+
+    struct AdventureLevel {
+        Metadata Metadata;
+        Monster Monster;
         GameObjects.ItemRequirement Requirements;
         IGameRewards.Reward Rewards;
-        GameObjects.Element damageType;
     }
 
 }
