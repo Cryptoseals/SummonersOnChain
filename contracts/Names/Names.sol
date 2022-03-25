a8a50a369ef0442289c8e8ff0b04be7c4b05368a
executionreverted <canersevince3@gmail.com>
2022-03-25 08:45:28 +0300

    set summoner name, name views, events

--- contracts/Names/Names.sol
+++ contracts/Names/Names.sol
@@ -2,6 +2,8 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 import "../Core/Navigator/InitNavigator.sol";
+import "../Interfaces/Core/Navigator/INavigator.sol";
+import "../Interfaces/Fungibles/Common/IFungibleInGameToken.sol";
 
 pragma solidity ^0.8.0;
 
@@ -11,9 +13,12 @@ contract Names is
     InitNavigator,
     ERC721EnumerableUpgradeable
 {
-    mapping(uint256 => uint256) public NameToSummoner;
-    mapping(string => uint) public NamesToToken;
+    uint256 public constant GOLD_COST = 50e18;
+    mapping(uint256 => uint256) internal SummonerToName;
+    mapping(string => uint256) public NamesToToken;
+    mapping(uint => string) public TokenToName;
 
+    event ChangeName(uint summoner, uint nameId, string name);
     function initialize(
         address _navigator,
         string memory name,
@@ -27,9 +32,28 @@ contract Names is
         external
         onlyGameContracts
     {
-        uint256 nextToken = totalSupply()+1;
+        IFungibleInGameToken(contractAddress(INavigator.CONTRACT.GOLD))
+            .burnToken(player, GOLD_COST);
+        uint256 nextToken = totalSupply() + 1;
         require(NamesToToken[_name] != 0, "taken");
         NamesToToken[_name] = nextToken;
+        TokenToName[nextToken] = _name;
         _mint(player, nextToken);
+    }
+
+    function setSummonerName(uint256 summoner, uint256 nameToken)
+        external
+        senderIsSummonerOwner(summoner)
+    {
+        require(ownerOf(nameToken) == msg.sender, "must own");
+        SummonerToName[summoner] = nameToken;
+        emit ChangeName(summoner, nameToken, TokenToName[nameToken]);
+    }
+
+    function nameOf(uint summoner) external view returns(string memory _name) {
+        uint token = SummonerToName[summoner];
+        if(ownerOfSummoner(summoner) == ownerOf(token)) {
+            _name = TokenToName[token];
+        }
     }
 }
